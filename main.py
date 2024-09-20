f540998048595783caaa96ae315a22c694d3f44c
Delmy Bonilla <bonildel001@warren.k12.in.us>
2024-09-20 08:22:16 -0400

    fixed file

--- main.py
+++ main.py
@@ -1,16 +1,106 @@
-import pygame
+import pygame, random
 
+# Set display surface
 pygame.init()
 
+# Set display surface
 WINDOW_WIDTH = 1000
 WINDOW_HEIGHT = 400
 display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption("Feed The Dragon")
 
+#Set FPS and clock
+FPS = 60
+clock = pygame.time.Clock()
+
+# Set game value:  CONSTANT_NAME, value
+PLAYER_STARTING_LIVES = 5
+PLAYER_VELOCITY = 10
+COIN_STARTING_VELOCITY = 10
+COIN_ACCELERATION = 0.5
+BUFFER_DISTANCE = 100
+
+score = 0
+player_lives = PLAYER_STARTING_LIVES
+coin_velocity = COIN_STARTING_VELOCITY
+
+# SET COLORS
+GREEN = (0, 255, 0)
+DARK_GREEN = (10, 50, 10)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+
+#Set fonts
+font = (pygame.font.Font('AttackGraffiti.ttf', 32))
+
+#Set Text for Score
+score_text = font.render("Score: " + str(score), True, GREEN, DARK_GREEN)
+score_rect = score_text.get_rect()
+score_rect.topleft = (10, 10)
+
+#Set Text for Title (Similar to Score)
+title_text = font.render("Feed the Dragon", True, GREEN, WHITE)
+title_rect = title_text.get_rect()
+title_rect.centerx = WINDOW_WIDTH / 2
+title_rect.y = 10
+
+#Set Text for Lives (Similar to Score)
+lives_text = font.render("Lives: " + str(player_lives), True, GREEN, DARK_GREEN)
+lives_rect = lives_text.get_rect()
+lives_rect.topright = (WINDOW_WIDTH - 10, 10)
+
+#Set Text for Game Over (Similar to Score)
+game_over_text = font.render("GAME OVER", True, GREEN, DARK_GREEN)
+game_over_rect = game_over_text.get_rect
+game_over_center = (WINDOW_WIDTH // 2, WINDOW_HEIGHT // 2)
+
+#Set Text for Continue (Similar to Score)
+continue_text = font.render("Press any key to play again", True, GREEN, DARK_GREEN)
+continue_rect = continue_text.get_rect
+continue_center = (WINDOW_WIDTH // 2, WINDOW_HEIGHT // 2 + 32)
+
+# Set sounds and music
+coin_sound = pygame.mixer.Sound("coin_sound.wav")
+miss_sound = pygame.mixer.Sound("miss_sound.wav")
+miss_sound.set_volume(0.1)
+pygame.mixer.music.load("ftd_background_music.wav")
+
+# Set images
+player_image = pygame.image.load("dragon_right.png")
+player_rect = player_image.get_rect()
+player_rect.left = 32
+player_rect.centery = WINDOW_HEIGHT // 2
+
+coin_image = pygame.image.load("coin.png")
+coin_rect = coin_image.get_rect()
+coin_rect.x = WINDOW_WIDTH + BUFFER_DISTANCE
+coin_rect.y = 0
+
+pygame.mixer.music.play(-1, 0.0)
+
+
+
+
+# The main game loop
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+
+    # Fill the display
+    display_surface.fill(BLACK)
+
+    display_surface.blit(score_text, score_rect)
+    display_surface.blit(title_text, title_rect)
+    display_surface.blit(lives_text, lives_rect)
+    display_surface.blit(player_image, player_rect)
+    display_surface.blit(coin_image, coin_rect)
+
+
+    # Update display and tick the clock
+    pygame.display.update()
+    clock.tick(FPS)
 
 pygame.quit()
